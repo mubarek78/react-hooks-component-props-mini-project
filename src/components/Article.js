37c38d4e01b70347070f3e0b3f4f35f8963791e6
mubarek78 <shifamubarek@yahoo.com>
2022-06-14 17:43:11 -0600

    assignment complited

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,18 +1,29 @@
-
 import React from "react";
-import logo from "../assets/logo.svg"
-function Article({ title, date = "January 1, 1970", preview, minutes }) {
-    const amountOfCoffees=Math.round(minutes / 5);
-    let coffees=[];
-    for (let i =0; i<amountOfCoffees; i++){
-        coffees.push(<img src={logo} alt="time"></img>);
-    }    
-    return (
-        <article>
-            <h3>{title}</h3>
-            <small>{date}{coffees}{minutes} min read</small>
-            <p>{preview}</p>
-        </article>
-    );
+
+function makeEmojiList(minutes) {
+  const interval = minutes < 30 ? 5 : 10;
+  const emoji = minutes < 30 ? "☕️" : "🍱";
+
+  let emojis = "";
+  for (let i = 0; i < minutes; i += interval) {
+    emojis += emoji;
+  }
+
+  return emojis;
 }
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const emojis = makeEmojiList(minutes);
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} • {emojis} {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
 export default Article;
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,17 +1,17 @@
+import React from "react";
 import Article from "./Article";
 
-function ArticleList({list}) {
-   const listArray = list.map((post)=> {
-       return  (
-       <Article date = {post.date} title = {post.title} preview = {post.preview} minutes = {post.minutes}/>
-   )
-})
-    
-    return (
-        <main>
-            {listArray}
-        </main>
-        )
+function ArticleList({ posts }) {
+  const articles = posts.map((post) => (
+    <Article
+      key={post.id}
+      title={post.title}
+      date={post.date}
+      preview={post.preview}
+      minutes={post.minutes}
+    />
+  ));
+  return <main>{articles}</main>;
 }
 
 export default ArticleList;
